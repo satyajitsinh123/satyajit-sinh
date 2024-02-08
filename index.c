7805e73801ee8f4ef4bbbc36455488c47aac002c
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-02-08 13:15:00 +0530

    exam

---
+++ index.c
@@ -0,0 +1,51 @@
+//------------------------------------ que-1----------------------------------
+// #include <stdio.h>
+// sum(int) int 10 int 20 int main(a + b)
+// {
+//     int a = 0;
+//     int b = 0;
+
+//     for (int i = 0; i <= 5; i++)
+//     {
+//         printf("%d", a);
+//     }
+
+//     return 0;
+// }
+
+//------------------------------------ que-2----------------------------------
+
+#include <stdio.h>
+#include <string.h>
+struct book
+{
+    char reprecent employ[100] char name[100] char price[100]
+} struct main()
+{
+    struct bookn[10];
+    int name;
+    printf("enter book employ number");
+    scanf("%d", &employ);
+    for (int i = 0; i < bookn; i++)
+
+        printf("enter your name");
+    scanf("%s", &struct);
+    printf("enter your price");
+    scanf("%d", &price);
+
+    return 0;
+}
+
+//------------------------------------ que-3----------------------------------
+
+#include <stdio.h>
+
+int main(){
+int arr[5][5]={11,20,11,20,11}
+              {12,13,50,41,62}
+              {14,15,16,41,62}
+              {17,18,19,20,62}
+              {21,22,23,24,15}
+              for()
+    return 0;
+}
